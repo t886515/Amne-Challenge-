4831036a3c470c1efcaa85baf75875291f268b1c
Lina Yang <t886515@hotmail.com>
2017-11-06 12:10:02 -0600

    refactor substring selection process - process now run 200000 3000 in 39 secs

--- challenge.js
+++ challenge.js
@@ -4,7 +4,7 @@ Please select the targeted test file and the name of the output file here.
 */
 
 const fileRead = 'test2'
-const fileWrite = 'output2'
+const fileWrite = 'output7'
 
 /*
 =========
@@ -15,6 +15,7 @@ const fileWrite = 'output2'
 const fs = require('fs');
 
 const helper = function(subset) {
+
   let extractSubset = [];
   for (let x = 0; x < subset.length -1; x++) {
     if (subset[x] > subset[x+1]) {
@@ -25,10 +26,14 @@ const helper = function(subset) {
       extractSubset.push(0);
     }
   }
+  return extractSubset;
+}
+
+const evalDiff = (subsetDiff) => {
   let sum = 0;
   let prev = null;
   let count = 1;
-  for (let curr of extractSubset) {
+  for (let curr of subsetDiff) {
     if (prev !== null && prev !== curr) {
       sum += (count*(count+1)/2) * prev;
       count = 1;
@@ -42,13 +47,16 @@ const helper = function(subset) {
 }
 
 const challenge = function(total, window, array) {
+  let diffSubset = helper(array);
+  // console.log(diffSubset);
   let resultString = "";
-  for (let i = 0; i < total-(window-1); i++) {
+  for (let i = 0; i < diffSubset.length-(window-2); i++) {
     let subset = [];
-    for (let j = 0; j < window; j++) {
-      subset.push(array[i+j])
+    for (let j = 0; j < window-1; j++) {
+      subset.push(diffSubset[i+j])
     }
-    resultString += helper(subset) + '\n';
+    // console.log('how fast does this print', "number: ", i)
+    resultString += evalDiff(subset) + '\n';
   } 
   fs.writeFile(fileWrite, resultString, 'utf-8', function(err, data) {
       if (err) throw err;
